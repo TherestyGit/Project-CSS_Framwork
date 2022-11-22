6aeffb9cccfee0c005f842872be5a4fe5bd0788b
TherestyGit <alixwilmart23@outlook.com>
2022-11-22 16:24:40 +0100

    modif

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $(".dropdown-trigger").dropdown({ hover: true });
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
   });
 
 new WOW().init();
